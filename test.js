1a26533b5412903c853df52ef644bf9e710525b7
朱如洪 <zhu410289616@163.com>
2019-10-23 00:01:03 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 {
-"key1":"value1",
-"key2":"value2"
+	"status":true,
+	"code":200,
+	"data":
+	[
+	{"id":"1","unique_id":"15033024077728990250","title":"第一条"},
+	{"id":"2","unique_id":"15033024077728990250","title":"第二条"}
+	]
 }
