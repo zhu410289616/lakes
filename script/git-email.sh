721239ba5e30ff2f65764ccd8fdc34d3667c0f18
zhu410289616 <zhu410289616@163.com>
2024-06-16 22:33:26 +0800

    backup

---
+++ script/git-email.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="zhuruhong@bytedance.com"
+CORRECT_NAME="zhu410289616"
+CORRECT_EMAIL="zhu410289616@163.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
